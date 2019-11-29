49388ac20a77e1e800395ff94394dc337171df14
Eclypzz <jefsky65@hotmail.fr>
2019-11-29 14:24:03 +0100

    fix #3 Tests sur ScoreCalculateur

---
+++ src/test/java/miagem1/ScoreCalculateurTest.java
@@ -0,0 +1,40 @@
+package miagem1;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+public class ScoreCalculateurTest {
+    private QuestionAChoixMultiple questionAChoixMultiple;
+    private ScoreCalculateur scoreCalculateur;
+    @Before
+    public void setUp() throws Exception {
+
+         questionAChoixMultiple = new QuestionAChoixMultiple("q1",new ArrayList<Integer>(Arrays.asList(2,3,5)));
+        scoreCalculateur = new ScoreCalculateur();
+    }
+
+    @Test
+    public void calculeScoreToutesBonnesReponses() {
+        float  resScore = scoreCalculateur.calculeScore(new ArrayList<Integer>(Arrays.asList(2,3,5)),questionAChoixMultiple);
+
+        assertEquals(100f,resScore,0.01f);
+    }
+
+    @Test
+    public void calculeScore2BonnesReponses() {
+        float  resScore = scoreCalculateur.calculeScore(new ArrayList<Integer>(Arrays.asList(2,3)),questionAChoixMultiple);
+
+        assertEquals((2f*100f/3f),resScore,0.01f);
+    }
+
+    @Test
+    public void calculeScoreMauvaises() {
+        float  resScore = scoreCalculateur.calculeScore(new ArrayList<Integer>(Arrays.asList(1,4)),questionAChoixMultiple);
+        assertEquals(0f,resScore,0.01f);
+    }
+}
